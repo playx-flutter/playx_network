0debcd35db33b1c8724a61466e4c1e1e71366439
Basem Osama <engbasemosama@gmail.com>
2023-07-18 17:51:37 +0300

    update example

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -6,21 +6,30 @@ import 'package:playx_network_example/model/exception/custom_exception_message.d
 import 'model/Cat.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, });
-
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+//Base url that can be used for api client on whole app.
 const String _baseUrl = 'https://api.open-meteo.com/v1/';
+// weather endpoint
 const String _forecastEndpoint = 'forecast';
+
+//url to override the default base url with cats endpoint.
 const String _catsUrl = 'https://api.thecatapi.com/v1/images/search';
 
 class _MyHomePageState extends State<MyHomePage> {
+  //Message for displaying current weather temperature from api.
   String _weatherMsg = '';
-  List<Cat> _cats =[];
 
+  //List of cats to be displayed from api.
+  List<Cat> _cats = [];
+
+  // determines whether the app is loading or not
   bool _isLoading = false;
 
   //you should create only one instance of this network client to be used for the app depending on your use case.
@@ -28,6 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void initState() {
+    //Configure your network client based on your needs.
     _client = PlayxNetworkClient(
       // customize your dio options.
       dio: Dio(
@@ -51,68 +61,16 @@ class _MyHomePageState extends State<MyHomePage> {
         }
         return null;
       },
+      //Whether you want to show api error message or default message.
       shouldShowApiErrors: true,
       //creates custom exception messages to be displayed when error is received.
       exceptionMessages: const CustomExceptionMessage(),
     );
     super.initState();
 
+    //Get weahter and cats from api.
     getWeatherFromApi();
     getCatsFromApi();
-
-  }
-
-  Future<void> getWeatherFromApi() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    final result = await _client.get<Weather>(_forecastEndpoint,
-        query: {
-          'latitude': '30.04',
-          'longitude': '31.23',
-          'current_weather': 'true',
-        },
-        fromJson: Weather.fromJson);
-
-    result.when(success: (weather) {
-      setState(() {
-        _isLoading = false;
-        _weatherMsg = "${weather.currentWeather?.temperature ?? 0} C";
-      });
-    }, error: (error) {
-      //handle error here
-      _weatherMsg = "Error is : ${error.message}";
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-
-  Future<void> getCatsFromApi() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    final result = await _client.getList(_catsUrl,
-        query: {
-          'limit': '10',
-        },
-        fromJson: Cat.fromJson);
-
-    result.when(success: (cats) {
-      setState(() {
-        _isLoading = false;
-        _cats = cats;
-      });
-    }, error: (error) {
-      //handle error here
-      _weatherMsg = "Error is : ${error.message}";
-      setState(() {
-        _isLoading = false;
-      });
-    });
   }
 
   @override
@@ -142,23 +100,98 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: ListView.builder(
                           itemCount: _cats.length,
                           itemBuilder: (context, index) {
-                    return SizedBox(
-                      height: 200,
-                      child: Image.network(_cats[index].url ??'', fit: BoxFit.cover,),
-                    );
-                  }))
+                            return SizedBox(
+                              height: 200,
+                              child: Image.network(
+                                _cats[index].url ?? '',
+                                fit: BoxFit.cover,
+                              ),
+                            );
+                          }))
                 ],
               ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           getWeatherFromApi();
           getCatsFromApi();
-
         },
-        tooltip: 'Weather',
+        tooltip: 'Refresh',
         child: const Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  ///Perform GET Request to get [Weather] model from the API and return it's result.
+  Future<void> getWeatherFromApi() async {
+    //set state of loading to start loading.
+    setState(() {
+      _isLoading = true;
+    });
+
+    final result = await _client.get(_forecastEndpoint,
+        //your custom queries.
+        query: {
+          'latitude': '30.04',
+          'longitude': '31.23',
+          'current_weather': 'true',
+        },
+        //Function to convert response from json to weather model.
+        fromJson: Weather.fromJson);
+
+    result.when(
+        //The request was performed successfully and returned the weather model.
+        success: (weather) {
+      setState(() {
+        _isLoading = false;
+        _weatherMsg = "${weather.currentWeather?.temperature ?? 0} C";
+      });
+    },
+        //There was an error while performing the request and returned an instance of NetworkException.
+        error: (error) {
+      //handle error here
+      _weatherMsg = "Error is : ${error.message}";
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  ///Perform GET Request to get List of [Cat] from the API and return it's result.
+  ///If successful it returns list of cats.
+  ///If not successful it returns an instance of [NetworkException].
+  Future<void> getCatsFromApi() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final result = await _client.getList(_catsUrl,
+        query: {
+          'limit': '10',
+        },
+        fromJson: Cat.fromJson);
+
+    result.when(success: (cats) {
+      setState(() {
+        _isLoading = false;
+        _cats = cats;
+      });
+    }, error: (error) {
+      //handle error here
+      _weatherMsg = "Error is : ${error.message}";
+      setState(() {
+        _isLoading = false;
+      });
+    });
+
+    // We can map the result to another type like this example:
+    // As it converts lis of cats to list of cats image urls.
+    final NetworkResult<List<String?>> catImagesResult = result.map(success: (success) {
+      final data = success.data;
+      final images = data.map((e) => e.url).toList();
+      return NetworkResult.success(images);
+    }, error: (error) {
+      return NetworkResult<List<String?>>.error(error.error);
+    });
   }
 }
